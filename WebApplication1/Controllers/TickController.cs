e288fab4e8f113308f27901094218b1e47d3ae78
losev87 <losev.87@mail.ru>
2018-02-20 08:43:08 +0300

    work

---
+++ WebApplication1/Controllers/TickController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TickController : ApiController
+    {
+        private FxContext _context;
+
+        public TickController()
+        {
+            _context=new FxContext();
+        }
+
+        public int Post(string symbol, double close, long time)
+        {
+            var newQuantHasCome = SaveTick(symbol, close, time);
+            if (!newQuantHasCome) return 3;
+            var result = GetResult();
+            return result;
+        }
+
+        bool SaveTick(string symbol, double close, long time)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var date = origin.AddSeconds(time);
+            var symbolEntity = _context.Symbols.SingleOrDefault(s => s.Name == symbol);
+            if (symbolEntity == null)
+            {
+                symbolEntity = new Symbol(){Name = symbol};
+                _context.Symbols.Add(symbolEntity);
+                _context.SaveChanges();
+            }
+
+            _context.Ticks.Add(new Tick()
+            {
+                Id = Guid.NewGuid(),
+                SymbolId = symbolEntity.Id,
+                Close = close,
+                Time = date
+            });
+
+
+            return true;
+
+
+        }
+
+        int GetResult()
+        {
+            return 3;
+        }
+    }
+}
---
+++ WebApplication1/Models/FxContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FxContext: DbContext
+    {
+        public DbSet<Tick> Ticks { get; set; }
+        public DbSet<Quant> Quants { get; set; }
+        public DbSet<Symbol> Symbols { get; set; }
+        public DbSet<SymbolSettings> SymbolSettings { get; set; }
+        public FxContext() : base("DefaultConnection")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Quant>().HasKey(e => e.Id, cfg => cfg.IsClustered(false));
+            modelBuilder.Entity<Tick>().HasKey(e => e.Id, cfg => cfg.IsClustered(false));
+        }
+    }
+
+    public class Tick
+    {
+        public Guid Id { get; set; }
+        [Index("IX_Tick", 2, IsClustered = true)]
+        public int SymbolId { get; set; }
+        public virtual Symbol Symbol { get; set; }
+        public double Close { get; set; }
+        [Index("IX_Tick", 1, IsClustered = true)]
+        public DateTime Time { get; set; }
+    }
+    public class Quant
+    {
+        public Guid Id { get; set; }
+        [Index("IX_Quant", 2, IsClustered = true)]
+        public int SymbolId { get; set; }
+        public virtual Symbol Symbol { get; set; }
+        public double Close { get; set; }
+        [Index("IX_Quant", 1, IsClustered = true)]
+        public int Ind { get; set; }
+    }
+    public class SymbolSettings
+    {
+        public int Id { get; set; }
+        [Index("IX_SymbolSettings", 1, IsUnique = true)]
+        public int SymbolId { get; set; }
+        public virtual Symbol Symbol { get; set; }
+        public double QuantSize { get; set; }
+    }
+    public class Symbol
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
